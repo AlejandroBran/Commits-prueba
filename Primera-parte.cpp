7bdd782f48f834ebb5785509773db3c62b162c5d
AlejandroBran <00224824@uca.edu.sv>
2024-06-25 08:36:06 -0600

    commit 1

---
+++ Primera-parte.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    float prime;
+    float segun;
+
+    cout << "\n Ingrese el primer numero: ";
+    cin >> prime;
+
+    cout << "\n Ingrese el segundo numero: ";
+    cin >> segun;
+    
+
+
+
+
+
+    return 0;
+}
