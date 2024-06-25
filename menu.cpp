c059280e17dc569619f477c2c4b1d57d07d33c95
AlejandroBran <00224824@uca.edu.sv>
2024-06-25 09:13:13 -0600

    Declaracion de variable

---
+++ menu.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float prime;
+    float segun;
+    float result;
+
+    cout << "\n Ingrese el primer numero: ";
+    cin >> prime;
+
+    cout << "\n Ingrese el segundo numero: ";
+    cin >> segun;
+
+    result = prime / segun;
+}
